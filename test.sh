87ace78eeb5713bc98ae55597ae36833cfa74274
Nazarii Muzychuk <muzychuk.n@gmail.com>
2017-07-06 20:42:59 +0300

    Secure secrets

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+### Create test kubernetes cluster ###
+
 # Install docker, kubelet, kubectl, kubeadm
 apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
@@ -21,11 +23,27 @@ kubectl apply -f https://git.io/weave-kube-1.6
 # Enable scheduling pods on the master
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
-# Deploy app components
+### Deploy app components ###
+
+# Deploy PostgreSQL
 kubectl apply -f postgres-deployment.yaml
+
+# Build app docker image
 QUIZZER_REPO=https://github.com/nmuzychuk/quizzer.git
 docker build -t quizzer ${QUIZZER_REPO}
+
+# Upload secrets
+SECRET_KEY_BASE=$(date | sha256sum | cut -d' ' -f1)
+DATABASE_URL=postgresql://postgres@postgres/quizzer_production
+
+kubectl create secret generic quizzer \
+  --from-literal=secret_key_base="${SECRET_KEY_BASE}" \
+  --from-literal=database_url="${DATABASE_URL}"
+
+# Deploy app
 kubectl apply -f quizzer-deployment.yaml
+
+# Expose
 kubectl expose deployment quizzer --type=NodePort
 
 sleep 30
